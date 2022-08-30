15fd895f494bba98ed4d0557219f335e0bc33f2f
Maxim <superametist7@gmail.com>
2022-08-30 18:37:20 +0300

    для поиска добавил кнопку очистить

--- src/components/TasksFilter/TasksFilter.jsx
+++ src/components/TasksFilter/TasksFilter.jsx
@@ -1,16 +1,22 @@
 import MySelect from '../UI/select/MySelect';
 import MyInput from '../UI/input/MyInput';
+import MyButton from '../UI/button/MyButton';
 import './TasksFilter.scss';
 
 const TasksFilter = ({ filter, setFilter }) => {
    return (
       <div className="tasks-filter">
-         <MyInput
-            // className="tasks-filter__input"
-            value={filter.query}
-            onChange={(e) => setFilter({ ...filter, query: e.target.value })}
-            placeholder="Поиск..."
-         />
+         <div className="tasks-filter__search">
+            <MyInput
+               // className="tasks-filter__input"
+               value={filter.query}
+               onChange={(e) => setFilter({ ...filter, query: e.target.value })}
+               placeholder="Поиск..."
+            />
+            <MyButton onClick={() => setFilter({ ...filter, query: '' })}>
+               Очистить
+            </MyButton>
+         </div>
          <MySelect
             className="tasks-filter__select"
             value={filter.sort}
